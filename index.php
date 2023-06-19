6d554674872ce07d10699472bdd686963839570e
frederick <frederickhidayat2003@gmail.com>
2023-06-19 22:01:38 +0700

    feat:landing page

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Learning</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+</head>
+<body>
+    <nav>
+        <div class="container">
+            <a href="/#" class="logo">E-Learning</a>
+            <ul class="navbar">
+                <li><a href="/#">courses</a></li>
+                <li><a href="/#">tutors</a></li>
+                <li><a href="/#">partners</a></li>
+                <li><a href="/#">admissions</a></li>
+                <li><a href="/#">contact</a></li>
+                <li><a href="/#" class="sign-up-button">Sign Up</a></li>
+            </ul>
+        </div>
+    </nav>
+    <main class="container">
+        <div class="main-div">
+            <h1>Learn without limits</h1>
+            <p>Start, switch, or advance your career with more than 5,800 courses, Professional Certificates, and degrees from world-class universities and companies.</p>
+            <div class="main-div-button">
+                <a class="main-div-join-now">Join Now</a>
+                <a class="main-div-browse-course">Browse Course</a>
+            </div>
+        </div>
+        <img src="images/image.jpg" class="main-image">
+    </main> 
+    
+</body>
+</html>
